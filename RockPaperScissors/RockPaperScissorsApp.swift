ca70d39e28c56556c454c24682612028031c4a5e
Carlos Eduardo Witte <cewitte@icloud.com>
2024-07-08 13:00:24 -0300

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Carlos Eduardo Witte on 08/07/24.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
